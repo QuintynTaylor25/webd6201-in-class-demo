ecea17f926e97937fb8b7cced42ae93e4b04f75d
Quintyn Taylor <quintyn.taylormcnutt@dcmail.ca>
2023-03-12 21:21:59 -0400

    Ice 8 Update

---
+++ ICE8/scripts/authguard.js
@@ -0,0 +1,10 @@
+(function(){
+        //check if user is logged in
+        if(!sessionStorage.getItem("user")){
+            //redirect the user to login.html
+            location.href = 'login.html'
+        }
+     
+    
+    
+})()
