963916e5d79bf88d9e6ad1d61816fd6b565a8c07
Quintyn Taylor <quintyn.taylormcnutt@dcmail.ca>
2023-03-26 15:51:34 -0400

    ICE 9 and 10 updated

---
+++ ICE9-10/scripts/authguard.js
@@ -0,0 +1,11 @@
+"use strict";
+(function(){
+    let protectedRoutes = [
+        'contact-list'
+    ];
+    if (protectedRoutes.indexOf(router.ActiveLink) > -1) {
+        if (!sessionStorage.getItem("user")) {
+            location.href = '/login';
+        }
+    }  
+})()
